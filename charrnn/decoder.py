203ebf15e943689abc4ff9e065de6e335e1d150e
Sang Han <jjangsangy@gmail.com>
2017-11-17 01:50:59 -0800

    select a random file rather than random sentence

--- charrnn/decoder.py
+++ charrnn/decoder.py
@@ -7,14 +7,24 @@ from __future__ import print_function
 import keras
 import sys
 import random
+import os
 
 import numpy as np
 
 from . cli import command_line
-from . text import get_text
 from . text import CHARS, IND_CHAR, CHAR_IND
 
 np.seterr(divide='ignore')
+
+
+def random_text(directory):
+    """
+    Reads a random file inside a directory of text files
+    """
+    datasets = [i for i in os.listdir(directory) if not i.startswith('.')]
+    filepath = '/'.join([directory, random.choice(datasets)])
+    with open(filepath, 'rt', encoding='utf-8') as dset:
+        return dset.read()
 
 
 def sample(preds, t=1.0):
@@ -40,7 +50,7 @@ def run(args):
     """
     Main entry point for outputting trained network
     """
-    text = get_text(args.datasets)
+    text = random_text(args.datasets)
     window_size = args.window
 
     model = keras.models.load_model(args.model)
